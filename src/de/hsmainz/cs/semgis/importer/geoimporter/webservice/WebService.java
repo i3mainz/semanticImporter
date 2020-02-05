8e0a11cd16ade042f1197b985ff6d8737ff2fa95
Timo Homburg <timo.homburg@hs-mainz.de>
2020-02-05 20:18:34 +0100

    Update WebService.java

--- src/de/hsmainz/cs/semgis/importer/geoimporter/webservice/WebService.java
+++ src/de/hsmainz/cs/semgis/importer/geoimporter/webservice/WebService.java
@@ -129,4 +129,50 @@ public class WebService {
 		} 
 	}
 	
+	@GET
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/getXSLTemplates")
+    public Response importWithMappingSchema() { 
+		final String dir = System.getProperty("user.dir");
+        System.out.println("current dir = " + dir); 
+        File folder=new File("xsl");
+        JSONArray result=new JSONArray();
+        for(String file:folder.list()) {
+        	if(file.endsWith(".xsl")) {
+        		result.put(file);
+        	}
+        }
+        return Response.ok(result.toString()).type(MediaType.APPLICATION_JSON).build();
+	}
+	
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces({"text/ttl"})
+	@Path("/convertSchemaToOWL")
+    public Response importWithMappingSchema(@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail,
+			@QueryParam("xsl") String xsl,
+			@QueryParam("formatname") String formatname,
+			@DefaultValue("en") @QueryParam("language") String language,
+			@DefaultValue("") @QueryParam("namespace") String namespace) { 
+		final String dir = System.getProperty("user.dir");
+        System.out.println("current dir = " + dir); 
+        XSD2OWL xsd2owl=new XSD2OWL();
+		xsd2owl.transformercounter = 0;
+		String owlfile = "xsd/"+formatname+"/"+formatname+"_testx";
+		String xsdfile = "xsd/"+formatname+"/XPlanGML.xsd";
+		try {
+			xsd2owl.transform(xsdfile, owlfile, xsl, namespace, formatname, null, language);
+			xsd2owl.justCleanUp(owlfile + ".owl", xsdfile, owlfile + ".owl", formatname, namespace);
+			xsd2owl.integrateCodeList(new File("xsd/"+formatname+"/XPlanGML_CodeLists.xml"), owlfile + ".owl",
+					namespace, "xsl/aaacodelist2owl.xsl", XMLTypes.XPLAN, formatname);
+		} catch (TransformerFactoryConfigurationError | TransformerException | XPathExpressionException | SAXException | IOException | ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return Response.ok("").type("text/ttl").build();
+	}
+	
 }
